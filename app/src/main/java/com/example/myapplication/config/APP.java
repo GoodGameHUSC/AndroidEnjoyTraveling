9af117392c737c43a9043c9b085891f74195420e
Hung Pham <tanhunghue@gmail.com>
2019-12-12 09:09:06 +0700

    Change domain server

--- app/src/main/java/com/example/myapplication/config/APP.java
+++ app/src/main/java/com/example/myapplication/config/APP.java
@@ -1,5 +1,6 @@
 package com.example.myapplication.config;
 
 public class APP {
-    public static String API_BASE_URL = "https://mytravel.stdiohue.com/";
+//    public static String API_BASE_URL = "https://mytravel.stdiohue.com/";
+    public static String API_BASE_URL = "http://jmcr.stdiohue.com/";
 }
